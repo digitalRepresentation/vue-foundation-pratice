e5791d82f8396d22db48c41f115b2b9415c3fdb6
DESKTOP-292AOP7\김진영 <kinemura@naver.com>
2021-06-09 22:17:20 +0900

    component used

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    {{ str }}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
   data: function() {
     return {
       str: 'hi'
     }
+  },
+  components: {
+    'app-header': AppHeader
   }
 }
 </script>

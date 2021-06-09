0ae1ccd88de5f80c9942537fad45bbad58c3752e
DESKTOP-292AOP7\김진영 <kinemura@naver.com>
2021-06-09 23:59:08 +0900

    form-pratice

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password">
+      <input id="password" type="password" v-model="password">
     </div>
-    <button>login</button>
+    <button tyoe="submit">login</button>
   </form>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username, this.password);
+    }
+  }
 }
 </script>
 

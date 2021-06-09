73bc2050b59b7cb4d1a066ab9a3d6237b328f449
DESKTOP-292AOP7\김진영 <kinemura@naver.com>
2021-06-09 23:35:55 +0900

    vue-form ADD

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    hi
-  </div>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password">
+    </div>
+    <button>login</button>
+  </form>
 </template>
 
 <script>
